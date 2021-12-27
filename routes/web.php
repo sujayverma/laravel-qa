a5ab3995ec379bde2447a1f267b63ab9ff6e015c
ext_s.barma <sujay.barma@indusnet.co.in>
2021-12-27 12:44:21 +0530

    Display questions

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\QuestionsController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -20,3 +20,4 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::resource('/question', QuestionsController::class);
