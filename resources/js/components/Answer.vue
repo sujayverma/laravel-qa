501a6be79b0004639bb9e0a30a1afe49cd6e6f06
ext_s.barma <sujay.barma@indusnet.co.in>
2022-05-31 17:12:09 +0530

    Added iztoast plugin for edit and delete answers

--- resources/js/components/Answer.vue
+++ resources/js/components/Answer.vue
@@ -40,21 +40,47 @@ export default {
                 this.editing = false;
                 console.log(res);
                 this.bodyHtml = res.data.body_html;
-                alert(res.data.message);
+                this.$toast.success(res.data.message, 'Success', { timeout: 3000 });
             })
             .catch((err) => {
-                alert(err.response.data.message);
+                this.$toast.error(err.response.data.message,'Error', { timeout: 3000 });
                 console.log(err.response);
             })
         },
         destroy () {
-            if(confirm('Are you sure?'))
-            {
-                axios.delete(this.endpoint)
-                .then( (res) => {
-                    jQuery(this.$el).fadeOut(1000, () => { alert(res.data.message) });
-                });
-            }
+              this.$toast.question( 'Are you sure about this?', 'confirm', {
+                    timeout: 3000,
+                    close: false,
+                    overlay: true,
+                    displayMode: 'once',
+                    id: 'question',
+                    zindex: 999,
+                    title: 'Hey',
+                    position: 'center',
+                    buttons: [
+                        ['<button><b>YES</b></button>',  (instance, toast) => {
+                
+                             axios.delete(this.endpoint)
+                                .then( (res) => {
+                                    // alert(this.$el);
+                                    // this.$el.fadeout(3000);
+                                     this.$toast.success(res.data.message, 'Success', { timeout: 3000 });
+                                });
+                
+                        }, true],
+                        ['<button>NO</button>', function (instance, toast) {
+                
+                            instance.hide({ transitionOut: 'fadeOut' }, toast, 'button');
+                
+                        }],
+                    ],
+                    onClosing: function(instance, toast, closedBy){
+                        console.info('Closing | closedBy: ' + closedBy);
+                    },
+                    onClosed: function(instance, toast, closedBy){
+                        console.info('Closed | closedBy: ' + closedBy);
+                    }
+            });
         }
     }
 }
