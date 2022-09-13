3e12422089472519fedefbd4fe6676c00fd5c735
ext_s.barma <sujay.barma@indusnet.co.in>
2022-09-13 13:18:49 +0530

    Created Markdown componenet

---
+++ resources/js/components/MEditor.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="card">
+    <div class="card-header">
+      <ul class="nav nav-tabs card-header-tabs">
+        <li class="nav-item">
+          <a
+            class="nav-link active"
+            aria-current="true"
+            data-bs-toggle="tab"
+            data-bs-target="#write-tab-pane"
+            href="#write-tab-pane"
+            >Write</a
+          >
+        </li>
+        <li class="nav-item">
+          <a
+            class="nav-link"
+            data-bs-toggle="tab"
+            data-bs-target="#preview-tab-pane"
+            href="preview-tab-pane"
+            >Preview</a
+          >
+        </li>
+      </ul>
+    </div>
+    <div class="card-body tab-content">
+      <!-- slot is like placeholder in vue js. This will be replaced by data from parent component.In this case we will eplace it with text area -->
+      <div class="tab-pane active" role="tabpanel" id="write-tab-pane">
+        <slot></slot>
+      </div>
+      <div class="tab-pane" role="tabpanel" id="preview-tab-pane">Preview</div>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  props: ["body"],
+};
+</script>
--- resources/js/components/Question.vue
+++ resources/js/components/Question.vue
@@ -1,118 +1,152 @@
 <template>
-    <div class="row justify-content-center">
-        <div class="col-md-126">
-            <div class="card">
-                <form class="card-body" v-if="editing" @submit.prevent="update">
-                    <div class="card-title">
-                        <input type="text" class="form-control form-control-lg" name="title" v-model="title" />
-                        
-                    </div>
-                <hr>
-                    <div class="d-flex bd-highlight">
-                        <div class="p-2 flex-grow-1 bd-highlight"> 
-                            <div class="form-group">
-                                <textarea class="form-control" v-model="body" rows="10" required></textarea>
-                            </div>
-                            <div class="mt-3">
-                                <button  type="submit" class="btn btn-lg btn-outline-primary" :disabled="IsValid">Update</button>
-                                <button @click = "cancel" class="btn btn-lg btn-outline-secondary">Cancel</button>
-                            </div>
-                        </div>
-                    </div>
-                </form>
-
-                <div class="card-body" v-else>
-                    <div class="card-title">
-                        <div class="d-flex align-items-center" style="align-items: center">
-                            <h2>{{title}}</h2>
-                            <div class="ml-auto" style="margin-left: auto">
-                                <a href="/question" class="btn btn-outline-secondary">Back to All Questions</a>
-                            </div>
-                        </div>
-                        
-                    </div>
-                <hr>
-                    <div class="d-flex bd-highlight">
-                        <div class="p-2 flex-grow-2 bd-highlight">
-                            <vote :model="question" name="question"></vote>
-                        </div>
-                    <div class="p-2 flex-grow-1 bd-highlight"> 
-                                <div v-html="bodyHtml"></div>
-                                <div class="ml-auto">
-                                    <a v-if="authorize('modify',question)" @click.prevent="edit" class="btn btn-sm btn-outline-info">Edit</a>
-                                    <button v-if="authorize('deleteQuestion',question)" @click.prevent="destroy" class="btn btn-sm btn-outline-danger">Delete</button>
-                                </div>
-                                <div class="float-end">
-                                    <user-info :model="question" label="Asked by"></user-info>
-                                </div>
-                    </div>
-                    </div>
-                </div>
+  <div class="row justify-content-center">
+    <div class="col-md-126">
+      <div class="card">
+        <form class="card-body" v-if="editing" @submit.prevent="update">
+          <div class="card-title">
+            <input
+              type="text"
+              class="form-control form-control-lg"
+              name="title"
+              v-model="title"
+            />
+          </div>
+          <hr />
+          <div class="d-flex bd-highlight">
+            <div class="p-2 flex-grow-1 bd-highlight">
+              <div class="form-group">
+                <m-editor :body="body">
+                  <textarea
+                    class="form-control"
+                    v-model="body"
+                    rows="10"
+                    required
+                  ></textarea>
+                </m-editor>
+              </div>
+              <div class="mt-3">
+                <button
+                  type="submit"
+                  class="btn btn-lg btn-outline-primary"
+                  :disabled="IsValid"
+                >
+                  Update
+                </button>
+                <button
+                  @click="cancel"
+                  class="btn btn-lg btn-outline-secondary"
+                >
+                  Cancel
+                </button>
+              </div>
             </div>
+          </div>
+        </form>
+
+        <div class="card-body" v-else>
+          <div class="card-title">
+            <div class="d-flex align-items-center" style="align-items: center">
+              <h2>{{ title }}</h2>
+              <div class="ml-auto" style="margin-left: auto">
+                <a href="/question" class="btn btn-outline-secondary"
+                  >Back to All Questions</a
+                >
+              </div>
+            </div>
+          </div>
+          <hr />
+          <div class="d-flex bd-highlight">
+            <div class="p-2 flex-grow-2 bd-highlight">
+              <vote :model="question" name="question"></vote>
+            </div>
+            <div class="p-2 flex-grow-1 bd-highlight">
+              <div v-html="bodyHtml"></div>
+              <div class="ml-auto">
+                <a
+                  v-if="authorize('modify', question)"
+                  @click.prevent="edit"
+                  class="btn btn-sm btn-outline-info"
+                  >Edit</a
+                >
+                <button
+                  v-if="authorize('deleteQuestion', question)"
+                  @click.prevent="destroy"
+                  class="btn btn-sm btn-outline-danger"
+                >
+                  Delete
+                </button>
+              </div>
+              <div class="float-end">
+                <user-info :model="question" label="Asked by"></user-info>
+              </div>
+            </div>
+          </div>
         </div>
+      </div>
     </div>
+  </div>
 </template>
 
 <script>
-import axios from 'axios';
-import Vote from './Vote.vue';
-import UserInfo from './UserInfo.vue';
-import modifications from '../mixins/modifications';
+import axios from "axios";
+import Vote from "./Vote.vue";
+import UserInfo from "./UserInfo.vue";
+import MEditor from "./MEditor.vue";
+import modifications from "../mixins/modifications";
 export default {
-    props: ['question'],
-    mixins: [modifications],
-    components: {Vote, UserInfo},
+  props: ["question"],
+  mixins: [modifications],
+  components: { Vote, UserInfo, MEditor },
 
-    data() {
-        return {
-            title: this.question.title,
-            body: this.question.body,
-            bodyHtml: this.question.body_html,
-            // editing: false, // defined in mixins.
-            id: this.question.id,
-            beforeEditCache: {}
-        }
+  data() {
+    return {
+      title: this.question.title,
+      body: this.question.body,
+      bodyHtml: this.question.body_html,
+      // editing: false, // defined in mixins.
+      id: this.question.id,
+      beforeEditCache: {},
+    };
+  },
+
+  computed: {
+    IsValid() {
+      return this.body.length < 10 || this.title.length < 10;
+    },
+    endpoint() {
+      return `${this.id}`;
+    },
+  },
+
+  methods: {
+    setEditCache() {
+      this.beforeEditCache = {
+        body: this.body,
+        title: this.title,
+      };
     },
 
-    computed: {
-        IsValid() {
-            return this.body.length < 10 || this.title.length < 10;
-        },
-        endpoint() {
-            return `${this.id}`;
-        }
+    getEditCache() {
+      this.body = this.beforeEditCache.body;
+      this.title = this.beforeEditCache.title;
     },
 
-    methods: {
-        setEditCache() {
-            this.beforeEditCache = {
-                body: this.body,
-                title: this.title
-            }
-        },
-
-        getEditCache() {
-            this.body = this.beforeEditCache.body;
-            this.title = this.beforeEditCache.title;
-        },
-         
-        payload() {
-            return {
-                body: this.body,
-                title: this.title
-            }
-        },
-
-        delete () {
-                    axios.delete(this.endpoint)
-                    .then( ({data}) => {
-                        this.$toast.success(data.message, "Success", { timeout: 2000})
-                    });
-                    
-                    setTimeout(() => {
-                    window.location.href= "/question";
-                    }, 3000);
-        }
+    payload() {
+      return {
+        body: this.body,
+        title: this.title,
+      };
     },
-}
+
+    delete() {
+      axios.delete(this.endpoint).then(({ data }) => {
+        this.$toast.success(data.message, "Success", { timeout: 2000 });
+      });
+
+      setTimeout(() => {
+        window.location.href = "/question";
+      }, 3000);
+    },
+  },
+};
 </script>
